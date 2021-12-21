fc068dcf25d0a8e2e215cac3b2dbeba2ac195bdd
fanrk <442189385@qq.com>
2021-12-21 09:31:04 +0800

    token校验

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -44,11 +44,17 @@ export default {
 	},
 	onShow() {
 		// #ifdef APP-PLUS
-		var currentWebview = this.$scope.$getAppWebview() //此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效
-		setTimeout(function() {
-			wv = currentWebview.children()[0]
-			wv.evalJS('window.getData()')
-		}, 1000); //如果是页面初始化调用时，需要延时一下
+		if (uni.getStorageSync('token')) {
+			var currentWebview = this.$scope.$getAppWebview() //此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效
+			setTimeout(function() {
+				wv = currentWebview.children()[0]
+				wv.evalJS('window.getData()')
+			}, 1000); //如果是页面初始化调用时，需要延时一下
+		}else{
+			uni.reLaunch({
+				url: '/pages/public/applogin'
+			});
+		}
 		// #endif
 	},
 	methods: {
